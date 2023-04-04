50d2f9dc7553a4b1e0f64e1c7483a419d501a8dc
Aida <edigeeva.aika@mail.ru>
2023-04-04 16:05:18 +0600

    Added nav to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { NavLink } from "react-router-dom";
+
 export default function App(){
   return (
     <div className="App">
       <h1>Hello World</h1>
       <p>This is my diploma</p>
+
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
     </div>
-  )
+    
+  );
 }
