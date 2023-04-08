f50948debce2c00f99440ddc528942b33add1ddd
Aida <edigeeva.aika@mail.ru>
2023-04-08 15:35:04 +0600

    Download icon for header

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>Welcome to my dairy store "Belaya Reka"</h1>
+      <p>This my awesome dairy store "Belaya Reka" </p>
+      <p>We only use natural intedients to make our yummy dairy products</p>
+    </div> 
+  )
+}
