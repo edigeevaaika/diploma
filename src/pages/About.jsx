406e1cc443cb9cfc6414bf6662470baa34879cb8
Aida <edigeeva.aika@mail.ru>
2023-04-09 14:04:32 +0600

    added catalog of products

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,13 @@
+import "./Pages.css";
 export default function About() {
   return (
     <div className="About">
       <h1>About all products dairy store "Belaya Reka"</h1>
-      
-    </div> 
-  )
+      <h3>Our catalog:</h3>
+      <p className="about-products">
+        Sport, Milk, Kefir, Sour cream, Yogurt, Cottage cheese, Butter, Cheese,
+        Chalap, Powdered milk.
+      </p>
+    </div>
+  );
 }
