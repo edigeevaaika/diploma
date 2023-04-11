5718d3b9fa5a2031697cac032c9c459c138fe537
Aida <edigeeva.aika@mail.ru>
2023-04-11 13:47:50 +0600

    changed icon import Link

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 import "./Logo.css";
-import logo from "../../assets/vite.svg";
+import logo from "../../assets/logo.png";
 import { NavLink } from "react-router-dom";
 
 export default function Logo(){
