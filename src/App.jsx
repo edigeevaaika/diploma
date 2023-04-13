268d75371e7de45b463e3600e571101b53f9820c
Aida <edigeeva.aika@mail.ru>
2023-04-13 14:50:46 +0600

    added useEffect to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,37 @@ import About from "./Pages/About";
 import Contacts from "./Pages/Contacts";
 import Delivery from "./Pages/Delivery";
 import Category from "./Pages/Сategory";
+import { createContext, useEffect, useState } from "react";
+import { getDoc, getDocs } from "firebase/firestore";
+import { categoryCollection } from "./Firebase";
 
+export const AppContext = createContext({
+  categories: []
+});
 
 export default function App() {
+  const [categories,setCategories] = useState([]);
+  useEffect(() => {
+    // получить категории из списка категорий
+    getDocs(categoryCollection).then((snapshot) => {
+      // категории будут храниться в snapshop.docs
+      const newCategories = [];
+      // coздать массив для категорий
+      snapshot.docs.forEach((doc) => {
+        //doc = категория
+        // заполнить массив данными из списка категорий
+        const category = doc.data();
+        category.id = doc.id;
+
+        newCategories.push(category);
+      });
+      // задать новый массив как состояние компонента
+      setCategories(newCategories);
+    });
+  }, []);
   return (
     <div className="App">
+      <AppContext.Provider value={{categories}}>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,6 +45,7 @@ export default function App() {
           <Route path="/category/:path" element={<Category/>} />
         </Routes>
       </Layout>
+      </AppContext.Provider>
     </div>
   );
 }
