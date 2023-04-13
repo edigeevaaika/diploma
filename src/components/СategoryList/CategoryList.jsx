766e3e8d0c63b2be78d3e4b1dfe04de533f1ff5d
Aida <edigeeva.aika@mail.ru>
2023-04-13 14:48:24 +0600

    cut useEffect from CategoryList

--- src/components/СategoryList/CategoryList.jsx
+++ src/components/СategoryList/CategoryList.jsx
@@ -1,30 +1,11 @@
-import { getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { categoryCollection } from "../../Firebase";
+import { AppContext } from "../../App";
 import "./CategoryList.css";
 
 export default function CategoryList() {
-  const [categories, setCategories] = useState([]);
+  const { categories } = useContext(AppContext);
   //выполнить эту функцую только один раз
-  useEffect(() => {
-    // получить категории из списка категорий
-    getDocs(categoryCollection).then((snapshot) => {
-      // категории будут храниться в snapshop.docs
-      const newCategories = [];
-      // coздать массив для категорий
-      snapshot.docs.forEach((doc) => {
-        //doc = категория
-        // заполнить массив данными из списка категорий
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-      // задать новый массив как состояние компонента
-      setCategories(newCategories);
-    });
-  }, []);
 
   const output = categories.map((category) => (
     <li key={category.id}>
