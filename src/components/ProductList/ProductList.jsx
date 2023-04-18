e20ecf61085a5ad0a79e6bc640789e93043abd43
Aida <edigeeva.aika@mail.ru>
2023-04-18 23:03:50 +0600

    added category.id & import AddToCart in files ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,16 +2,21 @@ import { useContext } from "react";
 import "./productList.css";
 import { AppContext } from "../../App";
 import { Link } from "react-router-dom";
+import AddToCart from "../AddToCart/AddToCart";
 
-export default function ProductList() {
+export default function ProductList({ category }) {
+  const { cart, setCart } = useContext(AppContext);
   const { products } = useContext(AppContext);
-
-  const output = products.map((product) => (
-    <div className="Product" key={product.id}>
-      <img src={product.picture} alt={product.name} />
-      <Link to={"/product/" + product.path}>{product.name}</Link>
-      <span>{product.price} som</span>
-    </div>
-  ));
+  console.log(products);
+  const output = products
+    .filter((product) => product.category === category.id)
+    .map((product) => (
+      <div className="Product" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <Link to={"/product/" + product.path}>{product.name}</Link>
+        <span>{product.price} som</span>
+        <AddToCart product={product}/>
+      </div>
+    ));
   return <div className="ProductList">{output}</div>;
 }
