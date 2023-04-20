98fd4ace2686e667f24e31c96a0fdb88f7c15885
Aida <edigeeva.aika@mail.ru>
2023-04-20 15:46:21 +0600

    Added Authetification for file Firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { collection, getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need 
+import { initializeApp } from "firebase/app"; 
+import { collection, getFirestore } from "firebase/firestore" 
+import {GoogleAuthProvider, getAuth, onAuthStateChanged, signOut, signInWithPopup} from "firebase/auth"; 
+// TODO: Add SDKs for Firebase products that you want to use 
+// https://firebase.google.com/docs/web/setup#available-libraries 
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,9 +15,17 @@ const firebaseConfig = {
   appId: "1:1038868746531:web:3a49b20e9130d27e43fc96",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-//получить список категорий (коллекция документов).
-export const categoryCollection = collection(db, "categories");
-export const productsCollection = collection(db, "products");
+// Initialize Firebase 
+const app = initializeApp(firebaseConfig); 
+const db = getFirestore(app); 
+const auth = getAuth (app); 
+ 
+// получить список категорий (коллекция документов). 
+export const categoryCollection = collection(db, 'categories'); 
+export const productCollection = collection(db, 'products'); 
+ 
+ 
+const provider = new GoogleAuthProvider (); 
+export const logIn = () => signInWithPopup  (auth, provider); 
+export const logOut = () => signOut (auth); 
+export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
