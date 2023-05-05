d002ba3766f1c27d7dfb9ab8035e7f4fa717aff6
Aida <edigeeva.aika@mail.ru>
2023-05-06 00:16:45 +0600

    Submitted the file Auth.jsx & added some features

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import "./Auth.css";
+import { AppContext } from "../../App";
+import { logIn, logOut } from "../../firebase";
+import { Link } from "react-router-dom";
+
+export default function Auth() {
+  const { user } = useContext(AppContext);
+
+  // показывается гостю
+  let output = (
+    <span className="Login">
+      Guest <button onClick={logIn}>Sign in</button>
+    </span>
+  );
+  // показывается пользователю
+  if (user) {
+    output = (
+      <span className="Logout">
+        <Link to={"/orders"} className="OrderLink">{user.displayName}</Link>
+        <button onClick={logOut}>Sign out</button>
+      </span>
+    );
+  }
+
+  return <div className="Auth">{output}</div>;
+}
