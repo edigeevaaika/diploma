1472e5781bbc44e183d08af76be3a169c8958f7b
Aida <edigeeva.aika@mail.ru>
2023-05-06 00:31:14 +0600

    Added a function & imported in folder NotFound.jsx

--- src/pages/Сategory.jsx
+++ src/pages/Сategory.jsx
@@ -2,31 +2,39 @@ import { useContext } from "react";
 import { useMatch } from "react-router-dom";
 import { AppContext } from "../App";
 import ProductList from "../components/ProductList/ProductList";
+import NotFound from "./NotFound";
 
 export default function Category() {
-  //деструктизация
+  // деструктизацтия
   const { params } = useMatch("/category/:path");
   const { categories } = useContext(AppContext);
   // простой метод
-  // const mach = useMatch("/category/:path");
+  // const match = useMatch("/category/:path");
   // match.params.path;
 
+  // найти категорию где путь категории совпадает с путем в браузере
   const category = categories.find(
-    (category) => params.path === category.path
+    category => params.path === category.path,
     /*
-    function(category){
-      if (params.path === category.path){
+    function(category) {
+      if (params.path === category.path) {
         return true;
-        else {
-          return false;
-        }
       }
-      */
+      else {
+        return false;
+      }
+    }
+    */
   );
+
+  if (!category) {
+    return <NotFound />;
+  }
+
   return (
     <div className="Category">
-      <h1>{category ? category.name : "Loading ..."}</h1>
-      <ProductList  category={category}/>
+      <h1>{category ? category.name : "Loading..."}</h1>
+      <ProductList category={category} />
     </div>
   );
 }
