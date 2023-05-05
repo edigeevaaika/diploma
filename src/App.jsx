00d0c6b1310dc5eb9f4c95c6b260ea8c3fec65fb
Aida <edigeeva.aika@mail.ru>
2023-05-06 00:08:27 +0600

    Commented and added several features in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import {
+  onAuthChange,
+  onCategoriesLoad,
+  onProductsLoad,
+  onOrdersLoad,
+} from "./firebase";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contacts from "./pages/Contacts";
 import Delivery from "./pages/Delivery";
 import Category from "./pages/Сategory";
 import { createContext, useEffect, useState } from "react";
-import { getDocs } from "firebase/firestore";
-import { categoryCollection, productCollection } from "./Firebase";
+// import { getDocs } from "firebase/firestore";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import Product from "./pages/Product";
+import ThankYou from "./pages/ThankYou";
 
 export const AppContext = createContext({
   categories: [],
   products: [],
-
+  orders: [],
   // корзина
   cart: {},
   setCart: () => {},
+
+  user: null, //здесь будет храниться информация про пользователя
 });
 
 export default function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
+  // состояние которое хранит информацию пользователя
+  const [user, setUser] = useState(null);
 
   // корзина
   const [cart, setCart] = useState(() => {
@@ -37,48 +48,59 @@ export default function App() {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
-  // выполнить эту функцию только один раз
+  // // выполнить эту функцию только один раз
+  // useEffect(() => {
+  //   // получить категории из списка категорий
+  //   getDocs(categoryCollection).then((snapshot) => {
+  //     // категории будут храниться в snapshot.docs
+
+  //     // создать массив для категорий
+  //     const newCategories = [];
+  //     // заполнить массив данными из списка категорий
+  //     snapshot.docs.forEach((doc) => {
+  //       // doc = категория
+  //       const category = doc.data();
+  //       category.id = doc.id;
+
+  //       newCategories.push(category);
+  //     });
+  //     // задать новый массив как состояние комапо
+  //     setCategories(newCategories);
+  //   });
+
+  //   // получить продукты из списка продуктов
+  //   getDocs(productCollection).then((snapshot) => {
+  //     // продукты будут храниться в snapshot.docs
+
+  //     // создать массив для продуктов
+  //     const newProducts = [];
+  //     // заполнить массив данными из списка продвук
+  //     snapshot.docs.forEach((doc) => {
+  //       // doc = продукт
+  //       const product = doc.data();
+  //       product.id = doc.id;
+
+  //       newProducts.push(product);
+  //     });
+  //     // задать новый массив как состояние комапо
+  //     setProducts(newProducts);
+  //   });
   useEffect(() => {
-    // получить категории из списка категорий
-    getDocs(categoryCollection).then((snapshot) => {
-      // категории будут храниться в snapshot.docs
-
-      // создать массив для категорий
-      const newCategories = [];
-      // заполнить массив данными из списка категорий
-      snapshot.docs.forEach((doc) => {
-        // doc = категория
-        const category = doc.data();
-        category.id = doc.id;
-
-        newCategories.push(category);
-      });
-      // задать новый массив как состояние комапо
-      setCategories(newCategories);
-    });
-
-    // получить продукты из списка продуктов
-    getDocs(productCollection).then((snapshot) => {
-      // продукты будут храниться в snapshot.docs
-
-      // создать массив для продуктов
-      const newProducts = [];
-      // заполнить массив данными из списка продвук
-      snapshot.docs.forEach((doc) => {
-        // doc = продукт
-        const product = doc.data();
-        product.id = doc.id;
-
-        newProducts.push(product);
-      });
-      // задать новый массив как состояние комапо
-      setProducts(newProducts);
+    onCategoriesLoad(setCategories);
+    onProductsLoad(setProducts);
+    onOrdersLoad(setOrders);
+    onAuthChange((user) => {
+      if (user) {
+        user.isAdmin = user.email === "zumakadyrovanagima@gmail.com";
+      }
+      setUser(user);
     });
   }, []);
-  
   return (
     <div className="App">
-      <AppContext.Provider value={{ categories, products, cart,setCart }}>
+      <AppContext.Provider
+        value={{ categories, products, cart, setCart, user, orders }}
+      >
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
@@ -87,10 +109,12 @@ export default function App() {
             <Route path="/delivery" element={<Delivery />} />
             <Route path="/category/:path" element={<Category />} />
             <Route path="/product/:path" element={<Product />} />
+            <Route path="/thank-you" element={<ThankYou />} />
 
             <Route path="*" element={<NotFound />} />
 
-            <Route path="/cart" element={<Cart/>}/>
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/orders" element={<Orders />} />
           </Routes>
         </Layout>
       </AppContext.Provider>
