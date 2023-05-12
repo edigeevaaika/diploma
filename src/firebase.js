c482de5ec98ef3ea5963e77f28410a71d1d6366d
Aida <edigeeva.aika@mail.ru>
2023-05-12 12:37:25 +0600

    added imports & exports in firebase.jsx

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,10 @@
 // Import the functions you need from the SDKs you need 
-import { initializeApp } from "firebase/app"; 
-import { collection, getFirestore } from "firebase/firestore" 
-import {GoogleAuthProvider, getAuth, onAuthStateChanged, signOut, signInWithPopup} from "firebase/auth"; 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore, onSnapshot } from "firebase/firestore";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
 // TODO: Add SDKs for Firebase products that you want to use 
 // https://firebase.google.com/docs/web/setup#available-libraries 
 
@@ -15,17 +18,54 @@ const firebaseConfig = {
   appId: "1:1038868746531:web:3a49b20e9130d27e43fc96",
 };
 
-// Initialize Firebase 
-const app = initializeApp(firebaseConfig); 
-const db = getFirestore(app); 
-const auth = getAuth (app); 
- 
-// получить список категорий (коллекция документов). 
-export const categoryCollection = collection(db, 'categories'); 
-export const productCollection = collection(db, 'products'); 
- 
- 
-const provider = new GoogleAuthProvider (); 
-export const logIn = () => signInWithPopup  (auth, provider); 
-export const logOut = () => signOut (auth); 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+const auth = getAuth(app);
+export const storage = getStorage(app);
+
+// получить список категорий (коллекция документов).
+export const categoryCollection = collection(db, 'categories');
+export const productCollection = collection(db, 'products');
+export const orderCollection = collection(db, 'orders');
+
+const provider = new GoogleAuthProvider();
+export const logIn = () => signInWithPopup(auth, provider);
+export const logOut = () => signOut(auth);
 export const onAuthChange = (callback) => onAuthStateChanged(auth, callback);
+
+export const onCategoriesLoad = (callback) =>
+  onSnapshot(categoryCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onProductsLoad = (callback) =>
+  onSnapshot(productCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+export const onOrdersLoad = (callback) =>
+  onSnapshot(orderCollection, (snapshot) =>
+    callback(
+      snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }))
+    )
+  );
+  // отправка фотографии и получение ее url
+export const uploadProductPhoto = async (file) => {
+  const storageRef = ref(storage, `products/${file.name}`);
+  await uploadBytes(storageRef, file);
+
+  const url = await getDownloadURL(storageRef);
+  return url;
+};
