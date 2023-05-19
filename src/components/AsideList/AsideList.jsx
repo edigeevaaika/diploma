8a7521f4197197d2dd023e2d7f825101c81983f3
Aida <edigeeva.aika@mail.ru>
2023-05-19 13:59:49 +0600

    changed width for video in AsideList.jsx

--- src/components/AsideList/AsideList.jsx
+++ src/components/AsideList/AsideList.jsx
@@ -4,12 +4,17 @@ import video from "../../assets/video.mp4";
 export default function AsideList() {
   return (
     <div className="video">
-        <video width="800" height="400" controls="controls" autoplay="autoplay">
-          <source
-            src={video}
-            type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
-          ></source>
-        </video>
-      </div>
-  )
+      <video
+        width="100%"
+        controls="controls"
+        autoplay="autoplay"
+        loop="loop"
+      >
+        <source
+          src={video}
+          type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+        ></source>
+      </video>
+    </div>
+  );
 }
