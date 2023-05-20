077e3f844425dfa4aa573363c53de7e1009162d4
Aida <edigeeva.aika@mail.ru>
2023-05-20 19:24:48 +0600

    Added img for guest and for user in Auth.jsx file

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -10,14 +10,14 @@ export default function Auth() {
   // показывается гостю
   let output = (
     <span className="Login">
-      Guest <button onClick={logIn}>Sign in</button>
+      <img src="https://cdn.icon-icons.com/icons2/1369/PNG/512/-person_90382.png" alt="" /> <button onClick={logIn}>Sign in</button>
     </span>
   );
   // показывается пользователю
   if (user) {
     output = (
       <span className="Logout">
-        <Link to={"/orders"} className="OrderLink">{user.displayName}</Link>
+        <Link to={"/orders"} className="OrderLink"><img src="https://cdn.icon-icons.com/icons2/1369/PNG/512/-person_90382.png" alt="" /></Link>
         <button onClick={logOut}>Sign out</button>
       </span>
     );
