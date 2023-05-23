1fdf0283f7ef9064e62ac095b309bd7cff917904
Aida <edigeeva.aika@mail.ru>
2023-05-23 15:22:03 +0600

    added width for Logo in Logo.jsx

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo(){
   return (
     <div className="Logo">
       <NavLink to="/">
-        <img src={logo} alt="Website logo" />
+        <img width="50" src={logo} alt="Website logo" />
       </NavLink>
     </div>
   )
