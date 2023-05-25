e2da4e10b4c701abb0692529cd2ade208512cab1
Aida <edigeeva.aika@mail.ru>
2023-05-25 16:10:39 +0600

    added className "News" & p, h1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,20 @@ export default function Home() {
     <div className="Home">
       <h1>Welcome to my dairy store "Belaya Reka"</h1>
       <p>This my awesome dairy store "Belaya Reka" </p>
-      <p>We only use natural intedients to make our yummy dairy products We only do what we believe in. We love what we do and that's why we do it well.
-
-      Selling village products is a responsible process. After all, it is very important that they are produced in suitable conditions so that their beneficial properties are not lost during transportation and that our customers receive only fresh, high-quality food. When ordering farm products from us, you can be sure that we are honest and responsible partners, we always keep our word and care about quality.</p>
-    </div> 
-  )
+      <p>
+        We only use natural intedients to make our yummy dairy products We only
+        do what we believe in. We love what we do and that's why we do it well.
+        Selling village products is a responsible process. After all, it is very
+        important that they are produced in suitable conditions so that their
+        beneficial properties are not lost during transportation and that our
+        customers receive only fresh, high-quality food. When ordering farm
+        products from us, you can be sure that we are honest and responsible
+        partners, we always keep our word and care about quality.
+      </p>
+      <div className="News">
+        <h1>News</h1>
+        
+      </div>
+    </div>
+  );
 }
