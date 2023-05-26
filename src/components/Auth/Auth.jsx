d1976833a0204e6b0acf8b00e55a1f3f057d41ec
Aida <edigeeva.aika@mail.ru>
2023-05-26 11:50:55 +0600

    second try of code

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -3,26 +3,21 @@ import "./Auth.css";
 import { AppContext } from "../../App";
 import { logIn, logOut } from "../../firebase";
 import { Link } from "react-router-dom";
-import userIicon from "../../assets/contacts_icons/auth.jpg";
-import adminIcon from "../../assets/contacts_icons/admin.jpg";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
 
   // показывается гостю
   let output = (
-    <span className="Login">
-      <img src={userIicon} alt="" /> <button onClick={logIn}>Sign in</button>
+    <span>
+      Guest <button onClick={logIn}>Sign in</button>
     </span>
   );
   // показывается пользователю
   if (user) {
     output = (
-      <span className="Logout">
-        <Link to={"/orders"} className="OrderLink">
-          {" "}
-          <img src={userIicon} alt="" />{" "}
-        </Link>
+      <span>
+        <Link to="/orders">{user.displayName}</Link>
         <button onClick={logOut}>Sign out</button>
       </span>
     );
